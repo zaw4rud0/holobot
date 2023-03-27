3eb6246f54e01e01f113afeac7314f0301c19087
xHarlock <adrianmartinez11199@gmail.com>
2023-03-27 19:41:50 +0200

    SQL file to create a new database with all the required tables.

---
+++ src/main/resources/database/SQL/create-database.sql
@@ -0,0 +1,101 @@
+CREATE TABLE Blacklisted
+(
+    id      INTEGER,
+    user_id INTEGER,
+    reason  TEXT,
+    date    TEXT,
+    FOREIGN KEY (user_id) REFERENCES DiscordUsers,
+    PRIMARY KEY (id AUTOINCREMENT)
+);
+
+CREATE TABLE BlockedImages
+(
+    id      INTEGER,
+    url     TEXT,
+    user_id INTEGER,
+    date    TEXT,
+    reason  TEXT,
+    FOREIGN KEY (user_id) REFERENCES DiscordUsers,
+    PRIMARY KEY (id AUTOINCREMENT)
+);
+
+CREATE TABLE DiscordGuildUsers
+(
+    guild_id INTEGER,
+    user_id  INTEGER,
+    FOREIGN KEY (guild_id) REFERENCES DiscordGuilds,
+    FOREIGN KEY (user_id) REFERENCES DiscordUsers
+);
+
+CREATE TABLE DiscordGuilds
+(
+    guild_id   INTEGER,
+    guild_name TEXT,
+    owner_id   INTEGER,
+    FOREIGN KEY (owner_id) REFERENCES DiscordUsers (user_id),
+    PRIMARY KEY (guild_id)
+);
+
+CREATE TABLE DiscordNicknames
+(
+    id       INTEGER,
+    nickname TEXT,
+    user_id  INTEGER,
+    guild_id INTEGER,
+    FOREIGN KEY (guild_id) REFERENCES DiscordGuilds,
+    FOREIGN KEY (user_id) REFERENCES DiscordUsers,
+    PRIMARY KEY (id AUTOINCREMENT)
+);
+
+CREATE TABLE DiscordUsers
+(
+    user_id       INTEGER,
+    username      TEXT,
+    discriminator TEXT,
+    is_bot        TEXT,
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE Emotes
+(
+    emote_id    INTEGER,
+    emote_name  TEXT,
+    created_at  TEXT,
+    image_url   TEXT,
+    is_animated TEXT,
+    PRIMARY KEY (emote_id)
+);
+
+CREATE TABLE Waifu
+(
+    id    TEXT,
+    tag   TEXT,
+    title TEXT,
+    PRIMARY KEY (id AUTOINCREMENT)
+);
+
+CREATE TABLE Submissions
+(
+    id         INTEGER,
+    type       TEXT,
+    user_id    INTEGER,
+    text       TEXT,
+    date       TEXT,
+    guild_id   INTEGER,
+    channel_id INTEGER,
+    FOREIGN KEY (user_id) references DiscordUsers,
+    FOREIGN KEY (guild_id) references DiscordGuilds,
+    PRIMARY KEY (id AUTOINCREMENT)
+);
+
+CREATE TABLE XkcdComics
+(
+    id    INTEGER,
+    title TEXT,
+    alt   TEXT,
+    img   TEXT,
+    day   INTEGER,
+    month INTEGER,
+    year  INTEGER,
+    PRIMARY KEY (id)
+)
