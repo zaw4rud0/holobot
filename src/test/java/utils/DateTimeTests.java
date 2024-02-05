7e053afff228953f7295ba4a9bd19725f59cdc1b
zaw4rud0 <adrianmartinez11199@gmail.com>
2024-02-05 23:45:07 +0100

    Added some more tests

--- src/test/java/utils/DateTimeTests.java
+++ src/test/java/utils/DateTimeTests.java
@@ -3,6 +3,7 @@ package utils;
 import dev.zawarudo.holo.utils.DateTimeUtils;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
@@ -11,11 +12,41 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class DateTimeTests {
 
     @Test
-    void testDateTimeFormat1() {
+    void testAmericanFormatWithTimezone() {
         String input = "February 26, 2024 23:59 (UTC+8)";
 
         ZonedDateTime zonedDateTime = ZonedDateTime.of(2024, 2, 26, 23, 59, 0, 0, ZoneId.of("UTC+8"));
         long expected = zonedDateTime.toInstant().toEpochMilli();
+
+        assertEquals(expected, DateTimeUtils.convertToMillis(input));
+    }
+
+    @Test
+    void testAmericanFormat() {
+        String input = "February 26, 2024 23:59";
+
+        LocalDateTime localDateTime = LocalDateTime.of(2024, 2, 26, 23, 59);
+        long expected = localDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+
+        assertEquals(expected, DateTimeUtils.convertToMillis(input));
+    }
+
+    @Test
+    void testAmericanFormatWithoutTime() {
+        String input = "February 26, 2024";
+
+        LocalDateTime localDateTime = LocalDateTime.of(2024, 2, 26, 0, 0);
+        long expected = localDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+
+        assertEquals(expected, DateTimeUtils.convertToMillis(input));
+    }
+
+    @Test
+    void testEuropeanFormat() {
+        String input = "26. February 2024 23:59";
+
+        LocalDateTime localDateTime = LocalDateTime.of(2024, 2, 26, 23, 59);
+        long expected = localDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
 
         assertEquals(expected, DateTimeUtils.convertToMillis(input));
     }
