b8f51bb7b32328b6e0b5e1ff4addcb8df1a0416f
zaw4rud0 <adrianmartinez11199@gmail.com>
2024-02-13 21:59:54 +0100

    Added examples

--- src/main/java/dev/zawarudo/holo/utils/DateTimeUtils.java
+++ src/main/java/dev/zawarudo/holo/utils/DateTimeUtils.java
@@ -137,13 +137,37 @@ public final class DateTimeUtils {
      */
     public enum DiscordTimestamp {
 
+        /**
+         * Example: 13 February 2024 21:57
+         */
         DEFAULT("<t:%d>"),
+        /**
+         * Example: 21:57
+         */
         SHORT_TIME("<t:%d:t>"),
+        /**
+         * Example: 21:57:00
+         */
         LONG_TIME("<t:%d:T>"),
+        /**
+         * Example: 13/02/24
+         */
         SHORT_DATE("<t:%d:d>"),
+        /**
+         * Example: 13 February 2024
+         */
         LONG_DATE("<t:%d:D>"),
+        /**
+         * Example: 13 February 2024 21:57
+         */
         SHORT_DATE_TIME("<t:%d:f>"),
+        /**
+         * Example: Tuesday, 13 February 2024 21:57
+         */
         LONG_DATE_TIME("<t:%d:F>"),
+        /**
+         * Example: 2 years ago
+         */
         RELATIVE_TIME("<t:%d:R>");
 
         private final String format;
