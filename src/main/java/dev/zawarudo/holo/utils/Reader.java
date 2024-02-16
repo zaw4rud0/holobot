900b1e3bba234dc6494a22e948a1c2ffc2ad66f7
zaw4rud0 <adrianmartinez11199@gmail.com>
2024-02-16 22:23:11 +0100

    Removed unused method

--- src/main/java/dev/zawarudo/holo/utils/Reader.java
+++ src/main/java/dev/zawarudo/holo/utils/Reader.java
@@ -3,34 +3,15 @@ package dev.zawarudo.holo.utils;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Scanner;
 
 public final class Reader {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(Reader.class);
-
 	private Reader() {
-	}
-	
-	public static String readLine(String filePath) {
-		Scanner scanner = null;
-		try {
-			scanner = new Scanner(new File(filePath));
-		} catch (FileNotFoundException ex) {
-			if (LOGGER.isErrorEnabled()) {
-				LOGGER.error("File not found: {}", filePath, ex);
-			}
-		}
-		return scanner != null ? scanner.nextLine() : null;
 	}
 
 	public static JsonObject readJsonObject(String filepath) throws IOException {
