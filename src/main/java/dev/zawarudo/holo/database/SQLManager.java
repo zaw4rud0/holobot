05b8db7fb29b890f6f01d4b3becc986d89261f37
zaw4rud0 <adrianmartinez11199@gmail.com>
2024-02-17 11:46:26 +0100

    Small improvement

--- src/main/java/dev/zawarudo/holo/database/SQLManager.java
+++ src/main/java/dev/zawarudo/holo/database/SQLManager.java
@@ -13,32 +13,44 @@ import java.util.Map;
 public class SQLManager {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SQLManager.class);
-
     private static final Path SQL_DIRECTORY_PATH = Path.of("./src/main/resources/database/SQL/");
     private final Map<String, String> sqlStatements;
 
+    /**
+     * Initializes the SQLManager by loading SQL statements from the defined directory.
+     *
+     * @throws IOException If there is an issue accessing or reading the SQL files.
+     */
     public SQLManager() throws IOException {
         sqlStatements = loadSQLStatements();
-
-        if (LOGGER.isInfoEnabled()) {
-            LOGGER.info("Loaded {} SQL statements!", sqlStatements.size());
-        }
+        LOGGER.info("Loaded {} SQL statements!", sqlStatements.size());
     }
 
+    /**
+     * Retrieves a SQL statement by its name.
+     *
+     * @param name The name of the SQL file, with or without the ".sql" extension.
+     * @return The SQL statement as a string.
+     * @throws IllegalArgumentException If there is no SQL statement with the given name.
+     */
     public String getStatement(String name) {
+        String adjustedName = name.endsWith(".sql") ? name.substring(0, name.length() - 4) : name;
+        if (!sqlStatements.containsKey(adjustedName)) {
+            throw new IllegalArgumentException("There is no SQL file with name: " + name);
+        }
         return sqlStatements.get(name);
     }
 
     private Map<String, String> loadSQLStatements() throws IOException {
-        Map<String, String> sqlStatementsMap = new HashMap<>();
+        Map<String, String> statements = new HashMap<>();
         try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(SQL_DIRECTORY_PATH, "*.sql")) {
             for (Path filePath : directoryStream) {
                 String name = getFileNameWithoutExtension(filePath);
                 String content = Files.readString(filePath);
-                sqlStatementsMap.put(name, content);
+                statements.put(name, content);
             }
         }
-        return sqlStatementsMap;
+        return statements;
     }
 
     private String getFileNameWithoutExtension(Path filePath) {
