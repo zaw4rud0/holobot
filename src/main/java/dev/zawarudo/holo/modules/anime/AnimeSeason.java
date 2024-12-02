8cddfc55d1247cd0bea6fcb92ff32cde66019f36
zaw4rud0 <adrianmartinez11199@gmail.com>
2024-12-02 22:46:10 +0100

    Added files for anime season graphics

---
+++ src/main/java/dev/zawarudo/holo/modules/anime/AnimeSeason.java
@@ -0,0 +1,62 @@
+package dev.zawarudo.holo.modules.anime;
+
+import dev.zawarudo.holo.modules.jikan.JikanAPI;
+import dev.zawarudo.holo.modules.jikan.model.AbstractMedium;
+import dev.zawarudo.holo.modules.jikan.model.Anime;
+import dev.zawarudo.holo.modules.jikan.model.Broadcast;
+import dev.zawarudo.holo.modules.jikan.model.Season;
+import dev.zawarudo.holo.utils.DateTimeUtils;
+import dev.zawarudo.holo.utils.exceptions.APIException;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class AnimeSeason {
+
+    private static final String TARGET_TIME_ZONE = "Europe/Zurich";
+
+    public static void main(String[] args) throws APIException {
+        List<Anime> seasonalAnime = JikanAPI.getSeason(Season.WINTER, 2024);
+
+        // Sort by popularity
+        seasonalAnime.sort(Comparator.comparingInt(AbstractMedium::getPopularity));
+
+        Map<String, List<Anime>> map = seasonalAnime.stream()
+                .filter(anime -> {
+                    Broadcast b = anime.getBroadcast();
+                    return b.getString().isPresent() &&
+                            b.getDay().isPresent() &&
+                            b.getTime().isPresent() &&
+                            b.getTimeZone().isPresent();
+                })
+                .collect(Collectors.groupingBy(
+                        anime -> getFormattedDateString(
+                                DateTimeUtils.convertDate(anime.getStartDate(), TARGET_TIME_ZONE)
+                        ),
+                        TreeMap::new,
+                        Collectors.toList()
+                ));
+
+        System.out.println("Anime Releases Winter 2024");
+
+        System.out.println(map.keySet().size() + " days");
+
+        for (String key : map.keySet()) {
+            System.out.println(key);
+            map.get(key).stream()
+                    .peek(anime -> anime.changeBroadcastTimeZone(TARGET_TIME_ZONE))
+                    .forEach(System.out::println);
+            System.out.println();
+        }
+    }
+
+    // TODO: Pick cover of most popular anime for each day
+
+    private static String getFormattedDateString(String date) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.parse(date);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd");
+        return zonedDateTime.format(formatter);
+    }
+}
