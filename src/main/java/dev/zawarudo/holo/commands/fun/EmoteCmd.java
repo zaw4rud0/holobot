6f1089976330b694599b6f7db869ac72ecdb5a14
zaw4rud0 <adrianmartinez11199@gmail.com>
2024-12-15 01:15:52 +0100

    Implemented basic functionality for emote cmd

--- src/main/java/dev/zawarudo/holo/commands/fun/EmoteCmd.java
+++ src/main/java/dev/zawarudo/holo/commands/fun/EmoteCmd.java
@@ -4,10 +4,17 @@ import dev.zawarudo.holo.commands.AbstractCommand;
 import dev.zawarudo.holo.commands.CommandCategory;
 import dev.zawarudo.holo.modules.emotes.EmoteManager;
 import dev.zawarudo.holo.utils.annotations.Command;
+import net.dv8tion.jda.api.entities.Icon;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Webhook;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.emoji.CustomEmoji;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
 import java.sql.SQLException;
 import java.util.Optional;
 
@@ -24,6 +31,9 @@ public class EmoteCmd extends AbstractCommand {
 
     @Override
     public void onCommand(@NotNull MessageReceivedEvent event) {
+        Member caller = event.getMember();
+        if (caller == null) return;
+
         String emoteName = args[0];
 
         try {
@@ -34,9 +44,26 @@ public class EmoteCmd extends AbstractCommand {
                 return;
             }
 
+            deleteInvoke(event);
+            CustomEmoji emote = emojiOptional.get();
 
-        } catch (SQLException e) {
-            event.getChannel().sendMessage(e.getMessage());
+            Webhook webhook = prepareWebhook(event.getChannel().asTextChannel(), caller);
+            webhook.sendMessage(emote.getImageUrl()).queue();
+        } catch (SQLException | IOException e) {
+            event.getChannel().sendMessage(e.getMessage()).queue();
         }
+    }
+
+    private Webhook prepareWebhook(TextChannel channel, @NotNull Member member) throws IOException {
+        String avatarUrl = member.getEffectiveAvatarUrl();
+        Icon icon = getIcon(avatarUrl);
+        return channel.createWebhook("Webhook").setName(member.getEffectiveName()).setAvatar(icon).complete();
+    }
+
+    private Icon getIcon(String urlString) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) URI.create(urlString).toURL().openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+        return Icon.from(connection.getInputStream());
     }
 }
