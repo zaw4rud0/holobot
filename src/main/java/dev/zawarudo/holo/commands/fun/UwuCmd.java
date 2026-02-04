eaa159b7caa3384fd8c131b2063c85c3f91d7b76
adrmrt <adrian.e.martinez.ayuso@gmail.com>
2026-02-05 00:11:31 +0100

    refactor: uwu command

--- src/main/java/dev/zawarudo/holo/commands/fun/UwuCmd.java
+++ src/main/java/dev/zawarudo/holo/commands/fun/UwuCmd.java
@@ -1,60 +1,80 @@
 package dev.zawarudo.holo.commands.fun;
 
+import dev.zawarudo.holo.core.command.CommandContext;
+import dev.zawarudo.holo.core.command.ExecutableCommand;
 import dev.zawarudo.holo.utils.annotations.CommandInfo;
 import dev.zawarudo.holo.commands.AbstractCommand;
 import dev.zawarudo.holo.commands.CommandCategory;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.Random;
 
 @CommandInfo(name = "uwu",
-        description = "Uwuifies a text of your choice. You can also reply to a message to uwuify it.",
+        description = "Uwuify a text of your choice. You can also reply to a message to uwuify it.",
         usage = "<text>",
         category = CommandCategory.MISC)
-public class UwuCmd extends AbstractCommand {
+public class UwuCmd extends AbstractCommand implements ExecutableCommand {
 
     private static final Random rand = new Random();
 
     @Override
-    public void onCommand(@NotNull MessageReceivedEvent event) {
-        deleteInvoke(event);
+    public void execute(@NotNull CommandContext ctx) {
+        ctx.invocation().deleteInvokeIfPossible();
 
-        if (args.length == 0 && event.getMessage().getReferencedMessage() == null) {
-            sendErrorEmbed(event, "Please provide text or reply to a message!");
+        final List<String> args = ctx.args();
+        final Message invokingMessage = ctx.message().orElse(null);
+        final Message referenced = invokingMessage != null
+                ? invokingMessage.getReferencedMessage()
+                : null;
+
+        if (args.isEmpty() && referenced == null) {
+            ctx.reply().errorEmbed("Please provide text or reply to a message!");
             return;
         }
 
-        Message reply = event.getMessage().getReferencedMessage();
-        Message msg = reply != null ? reply : event.getMessage();
-
-        // Check for pings
-        if (hasPings(msg)) {
-            sendErrorEmbed(event, "No pings!");
+        // Choose which message to uwuify
+        Message msg = referenced != null ? referenced : invokingMessage;
+        if (msg == null) {
+            // Should never happen
+            ctx.reply().errorEmbed("Please provide text or reply to a message!");
             return;
         }
 
-        // No emotes
+        // Disallow pings
+        if (!msg.getMentions().getMembers().isEmpty()
+                || !msg.getMentions().getRoles().isEmpty()
+                || !msg.getMentions().getUsers().isEmpty()) {
+            ctx.reply().errorEmbed("No pings!");
+            return;
+        }
+
+        // No custom emojis
         if (!msg.getMentions().getCustomEmojis().isEmpty()) {
-            sendErrorEmbed(event, "I can't use custom emojis!");
+            ctx.reply().errorEmbed("I can't use custom emojis!");
             return;
         }
 
-        if (event.getMessage().getReferencedMessage() != null) {
-            String uwu = uwuify(event.getMessage().getReferencedMessage().getContentRaw());
-            sendText(event, uwu);
+        final String input;
+        if (referenced != null) {
+            input = referenced.getContentRaw();
         } else {
-            String uwu = uwuify(String.join(" ", args));
-            sendText(event, uwu);
+            input = String.join(" ", args);
         }
+
+        final String uwu = uwuify(input);
+        sendText(ctx, uwu);
     }
 
-    private void sendText(MessageReceivedEvent event, String text) {
+    private void sendText(CommandContext ctx, String text) {
         int index = 0;
         while (index < text.length()) {
-            String chunk = text.substring(index, Math.min(index + Message.MAX_CONTENT_LENGTH, text.length()));
-            event.getChannel().sendMessage(chunk).queue();
+            String chunk = text.substring(
+                    index,
+                    Math.min(index + Message.MAX_CONTENT_LENGTH, text.length())
+            );
+            ctx.reply().text(chunk);
             index += Message.MAX_CONTENT_LENGTH;
         }
     }
@@ -68,9 +88,11 @@ public class UwuCmd extends AbstractCommand {
                 .replace("uck", "uwq")
                 .replaceFirst("i", "i-i")
                 .replaceFirst("(?s)(.*)" + "i-i-i", "$1" + "i-i");
+
         if (rand.nextInt(10) <= 2) {
             result += " >-<";
         }
+
         if (rand.nextInt(10) <= 1) {
             result += " UwU";
         }
